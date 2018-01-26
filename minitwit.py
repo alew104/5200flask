455685474a16e3516a751c85c4cea3fe8a4489d7
Alex Lew <alew104@gmail.com>
2018-01-25 20:29:53 -0800

    trying this list thing i found on soverflow

--- minitwit.py
+++ minitwit.py
@@ -132,7 +132,7 @@ def public_timeline():
 def user_timeline(username):
     """Display's a users tweets."""
     profile_user = query_db('select * from user where username = ?',
-                            [username], one=True)
+                            (username,), one=True)
     if profile_user is None:
         abort(404)
     followed = False
