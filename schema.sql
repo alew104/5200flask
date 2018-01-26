697101fb4811fa2cfa17ca428895fb6795c3c8c5
EC2 Default User <ec2-user@ip-172-31-22-159.us-west-2.compute.internal>
2018-01-26 23:21:27 +0000

    update schema.sql back to original with max packet size

--- schema.sql
+++ schema.sql
@@ -1,6 +1,23 @@
+set max_allowed_packet=67108864
+
 drop table if exists user;
-create table user (user_id integer primary key autoincrement, username text not null, email text not null, pw_hash text not null);
+create table user (
+user_id integer primary key autoincrement,
+username text not null,
+email text not null,
+pw_hash text not null
+);
+
 drop table if exists follower;
-create table follower (who_id integer, whom_id integer);
+create table follower (
+who_id integer,
+whom_id integer
+);
+
 drop table if exists message;
-create table message (message_id integer primary key autoincrement, author_id integer not null, text text not null, pub_date integer);
+create table message (
+message_id integer primary key autoincrement,
+author_id integer not null,
+text text not null,
+pub_date integer
+);
