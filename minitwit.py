c4cf493552e4da064936397413f393e996c0e5fe
EC2 Default User <ec2-user@ip-172-31-22-159.us-west-2.compute.internal>
2018-01-26 03:51:16 +0000

    ran into error with formattin

--- minitwit.py
+++ minitwit.py
@@ -41,8 +41,8 @@ def get_db():
     top = _app_ctx_stack.top
     if not hasattr(top, 'db'):
         top.db = pymysql.connect(host=app.config['DATABASE'],user= app.config['DB_USER'],passwd= app.config['DB_PASS'],db= app.config['DB_NAME'], port=3306)
-        top.db.row_factory = pymysql.fetchone()
-    return top.db
+        #top.db.row_factory = pymysql.fetchone()
+    return top.db.cursor()
 
 
 @app.teardown_appcontext
