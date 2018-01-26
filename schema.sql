2c2d29c835749cfeb0279ab43222e997b9b2edaf
Alex Lew <alew104@gmail.com>
2018-01-26 15:28:36 -0800

    fix auto_increment -_-

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ set max_allowed_packet=67108864
 
 drop table if exists user;
 create table user (
-user_id int primary key autoincrement,
+user_id int primary key auto_increment,
 username varchar(40) not null,
 email varchar(40) not null,
 pw_hash varchar(255) not null
@@ -16,7 +16,7 @@ whom_id int
 
 drop table if exists message;
 create table message (
-message_id int primary key autoincrement,
+message_id int primary key auto_increment,
 author_id int not null,
 text varchar(255) not null,
 pub_date int
