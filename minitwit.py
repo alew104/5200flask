126be17659e2d5b8ec5057dd45fba7bd676e2202
Alex Lew <alew104@gmail.com>
2018-01-26 17:13:27 -0800

    fix user login

--- minitwit.py
+++ minitwit.py
@@ -80,7 +80,7 @@ def get_user_id(username):
     """Convenience method to look up the id for a username."""
     rv = query_db('select user_id from user where username = %s',
                   [username], one=True)
-    return rv[0] if rv else None
+    return rv['user_id'] if rv else None
 
 
 def format_datetime(timestamp):
