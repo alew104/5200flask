4079a61973d2251cde94b27ed161dd2119d2d93c
Alex Lew <alew104@gmail.com>
2018-01-28 15:26:40 -0800

    change primary key syntx

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 drop table if exists user;
 create table user (
-user_id int primary key auto_increment,
+user_id int auto_increment,
 username varchar(40) not null,
 email varchar(40) not null,
-pw_hash varchar(255) not null
+pw_hash varchar(255) not null,
+PRIMARY KEY ('user_id')
 );
 
 drop table if exists follower;
@@ -14,8 +15,9 @@ whom_id int
 
 drop table if exists message;
 create table message (
-message_id int primary key auto_increment,
+message_id int auto_increment,
 author_id int not null,
 text varchar(255) not null,
-pub_date int
+pub_date int,
+PRIMARY KEY ('message_id')
 );
